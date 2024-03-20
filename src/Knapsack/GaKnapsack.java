2e742eacfc0b825e190acd2d04edb2057c6a9f70
Emre <emrekarablt53@gmail.com>
2024-03-20 11:28:20 +0100

    GA- test module is ready

--- src/Knapsack/GaKnapsack.java
+++ src/Knapsack/GaKnapsack.java
@@ -7,32 +7,35 @@ public class GaKnapsack {
 
     public int populationSize ;
     public int geneLength ;
+    public int maxGen ;
     public double mutationRate ;
     ArrayList<IndividualKnapsack> population ;
-    public GaKnapsack(int populationSize , int geneLength, double mutationRate){
-
+    public GaKnapsack(ArrayList<IndividualKnapsack> population , int populationSize , int geneLength, double mutationRate, int maxGen){
+        this.maxGen = maxGen;
         this.populationSize = populationSize;
         this.mutationRate = mutationRate;
         this.geneLength = geneLength;
-        this.population = new ArrayList<>();
+        this.population = population;
         geneticAlgorithm();
     }
-    public void initializePopulation(){
+    public static ArrayList<IndividualKnapsack> initializePopulation( int populationSize , int geneLength){
+        ArrayList<IndividualKnapsack> pop = new ArrayList<>();
         for (int i = 0; i < populationSize; i++) {
             int[] genome = new int[geneLength];
             for (int j = 0; j < geneLength; j++) {
                 genome[j] = (int)(Math.round(Math.random()));
             }
             IndividualKnapsack individual = new IndividualKnapsack(genome);
-            population.add(individual);
+            pop.add(individual);
         }
+        return pop;
 
 
     }
     public void geneticAlgorithm(){
-        initializePopulation();
+
         int gen = 0 ;
-        while(!checkStoppingConditionReached(population, 0.90)){
+        while(!checkStoppingConditionReached(population, 0.90, gen)){
             ArrayList<IndividualKnapsack> matingPool = selection();
             population = generateNewGeneration(matingPool);
 
@@ -49,17 +52,17 @@ public class GaKnapsack {
                 .stream().map(IndividualKnapsack::getFitness).reduce(0,Integer::sum);
         return sum / (double)(populationSize);
     }
-    public boolean checkStoppingConditionReached(ArrayList<IndividualKnapsack> population , double percantage ){
+    public boolean checkStoppingConditionReached(ArrayList<IndividualKnapsack> population , double percantage, int generation ){
+        if( this.maxGen > 0){
+            return generation > maxGen;
+        }
         double averageFitness = this.averageFitness(population);
         double count = 0;
         for (IndividualKnapsack individual : population) {
             if(individual.getFitness() > averageFitness)
                 count++;
         }
-        if( count / populationSize >= percantage){
-            return true;
-        }
-        return false;
+        return count / populationSize >= percantage;
     }
 
     public IndividualKnapsack crossover(IndividualKnapsack mother , IndividualKnapsack father){
@@ -104,18 +107,23 @@ public class GaKnapsack {
 //        Knapsack.KnapsackItem b = new Knapsack.KnapsackItem(7,3);
 //        Knapsack.KnapsackItem c = new Knapsack.KnapsackItem(8,5);
         ArrayList<KnapsackItem> items = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                items.add(new KnapsackItem(i,j));
-            }
+        int numberOfItems = 2500 ;
+        for (int i = 0; i < numberOfItems; i++) {
+                items.add(new KnapsackItem(random_.nextInt(10), random_.nextInt(10)));
         }
+        Knapsack.items = items;
+        Knapsack.limit = 350;
+
+//        Knapsack knapsack = new Knapsack();
+//        System.out.println(knapsack.bruteForce(items.size()));
 //        items.add(a);
 //        items.add(b);
 //        items.add(c);
-        Knapsack.items = items;
-        Knapsack.limit = 100;
-        GaKnapsack ga = new GaKnapsack(100000,Knapsack.items.size(),0.001);
-
+        ArrayList<IndividualKnapsack> population = GaKnapsack.initializePopulation(10000, numberOfItems );
+        double[] mutation_rates = {0.00001 ,0.000025 , 0.00005 , 0.0001 , 0.0002,0.0005 };
+        for (double mutation_rate : mutation_rates) {
+            GaKnapsack ga = new GaKnapsack(population, population.size(), Knapsack.items.size(),  mutation_rate,500);
+        }
     }
 
 }
